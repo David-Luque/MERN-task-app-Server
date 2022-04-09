222deff79d91dc86496123896c3fc8d1e33f8d07
David L <dvdlq99@gmail.com>
2022-04-09 15:44:43 +0200

    updated delete and update project controllers. also task model

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -48,7 +48,6 @@ const getSingleProject = async (req, res)=>{
         const error = new Error("Not found");
         return res.status(404).json({ msg: error.message });
     }
-    
 
     if(project.owner.toString() !== req.user._id.toString()) {
         const error = new Error("Not valid");
@@ -58,65 +57,114 @@ const getSingleProject = async (req, res)=>{
 
 //update project
 const updatedProject = async (req, res)=>{
-    //check for errors
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-    }
+    // //check for errors
+    // const errors = validationResult(req);
+    // if(!errors.isEmpty()) {
+    //     return res.status(400).json({ errors: errors.array() })
+    // }
     
-    //extract project info
-    const { name } = req.body;
+    // //extract project info
+    // const { name } = req.body;
     
-    const newProject = {};
-    if(name) newProject.name = name;
+    // const newProject = {};
+    // if(name) newProject.name = name;
 
-    try {
-        //check project ID
-        let project = await Project.findById(req.params.id)
+    // try {
+    //     //check project ID
+    //     let project = await Project.findById(req.params.id)
         
-        //check if project exist
-        if(!project) {
-            return res.status(404).json({ msg: 'Project not found' });
-        }
+    //     //check if project exist
+    //     if(!project) {
+    //         return res.status(404).json({ msg: 'Project not found' });
+    //     }
 
-        //verify project owner 
-        if(project.owner.toString() !== req.user.id) {
-            return res.status(401).json({ msg: 'Unauthorized' })
-        }
+    //     //verify project owner 
+    //     if(project.owner.toString() !== req.user.id) {
+    //         return res.status(401).json({ msg: 'Unauthorized' })
+    //     }
 
-        //update
-        project = await Project.findByIdAndUpdate({ _id: req.params.id }, { $set: newProject }, { new: true })
-        res.json({ project });
+    //     //update
+    //     project = await Project.findByIdAndUpdate({ _id: req.params.id }, { $set: newProject }, { new: true })
+    //     res.json({ project });
+
+    // } catch (error) {
+    //     console.log(error)
+    //     res.status(500).send('There was an error on server')
+    // }
+
+    const { id } = req.params;
+
+    const project = Project.findById(id);
+
+    if(!project) {
+        const error = new Error("Not found");
+        return res.status(404).json({ msg: error.message });
+    }
+
+    if(project.owner.toString() !== req.user._id.toString()) {
+        const error = new Error("Not valid");
+        return res.status(401).json({ msg: error.message });
+    };
+
+    project.name = req.body.name || project.name;
+    project.description = req.body.description || project.description;
+    project.deadlineDate = req.body.deadlineDate || project.deadlineDate;
+    project.client = req.body.client || project.client;
+    
+    try {
+        const savedProject = project.save();
+        res.json(savedProject);
 
     } catch (error) {
         console.log(error)
-        res.status(500).send('There was an error on server')
     }
 };
 
 //delete project
 const deleteProject = async (req, res)=>{
-    try {
-        //check project ID
-        let project = await Project.findById(req.params.id)
+    // try {
+    //     //check project ID
+    //     let project = await Project.findById(req.params.id)
         
-        //check if project exist
-        if(!project) {
-            return res.status(404).json({ msg: 'Project not found' });
-        }
+    //     //check if project exist
+    //     if(!project) {
+    //         return res.status(404).json({ msg: 'Project not found' });
+    //     }
 
-        //verify project owner 
-        if(project.owner.toString() !== req.user.id) {
-            return res.status(401).json({ msg: 'Unauthorized' })
-        }
+    //     //verify project owner 
+    //     if(project.owner.toString() !== req.user.id) {
+    //         return res.status(401).json({ msg: 'Unauthorized' })
+    //     }
 
-        //delete the project
-        await Project.findOneAndRemove({ _id: req.params.id });
-        res.json({ msg: 'Project deleted' });
+    //     //delete the project
+    //     await Project.findOneAndRemove({ _id: req.params.id });
+    //     res.json({ msg: 'Project deleted' });
+
+    // } catch (error) {
+    //     console.log(error)
+    //     res.status(500).send('There was an error on server')
+    // }
+
+    const { id } = req.params;
+
+    const project = Project.findById(id);
+
+    if(!project) {
+        const error = new Error("Not found");
+        return res.status(404).json({ msg: error.message });
+    }
+
+    if(project.owner.toString() !== req.user._id.toString()) {
+        const error = new Error("Not valid");
+        return res.status(401).json({ msg: error.message });
+    };
+
+    try {
+        await project.deleteOne();
+        res.json({ msg: "Project deleted" });
 
     } catch (error) {
-        console.log(error)
-        res.status(500).send('There was an error on server')
+        console.log(error);
     }
 };
 
--- models/Tasks.js
+++ models/Tasks.js
@@ -1,7 +1,13 @@
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
+import mongoose from  'mongoose';
 
 const taskSchema = mongoose.Schema({
     name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
         type: String,
         required: true,
         trim: true
@@ -10,14 +16,29 @@ const taskSchema = mongoose.Schema({
         type: Boolean,
         default: false
     },
+    deadlineDate: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    priority: {
+        type: String,
+        required: true,
+        enum: ['low', 'medium', 'high']
+    },
     project: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Project'
     },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    }
+    // createdAt: {
+    //     type: Date,
+    //     default: Date.now()
+    // }
+}, {
+    timestamps: true
 });
 
-module.exports = mongoose.model('Task', taskSchema);
+//module.exports = mongoose.model('Task', taskSchema);
+const Task = mongoose.model('Task', taskSchema);
+export default Task;
+
